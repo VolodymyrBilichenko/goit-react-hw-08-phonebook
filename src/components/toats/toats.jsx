d44a3cf0fa7abf797c5a1e7eb4e56ebfaea11c69
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-03 18:36:49 +0200

    Create toats.jsx

---
+++ src/components/toats/toats.jsx
@@ -0,0 +1,20 @@
+
+import { ToastContainer } from "react-toastify"
+
+export const Toast = () => {
+    return (
+        <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
+        
+    )
+}
